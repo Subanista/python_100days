a01a6879cedbc8097bf8f4f1f42ef6e1cdeb9c00
Subanista Thaneswaran <88665143+Subanista@users.noreply.github.com>
2022-09-29 09:18:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Hello Replit")
+print("I am in the Day 1 of 100 days with pyhton")
+print("""
+day1 
+starts
+smoothly
+""")
+# Print("hi") it will give the name error, because Print is not defined
+# print"hi"   this will make syntax error means invalid syntax 
+#day1 challenge  
+firstName="Subanista"
+print(firstName)
+import datetime
+now=datetime.datetime.now()
+print(now)
+print(""" 
+i am signing up for 100 days of python challenge!
+I will make sure to spend time every day coding along, for a minimum of 10 minutes a day
+""")
+print(" i am feeling  \U0001F60A" ) # here i used emoji instead of type my mood
+print("you can follow my progress at replit.com/@subanista ")
+#done
